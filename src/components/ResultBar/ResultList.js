92387712a4bc868258aa7cf55b1463049795bb70
BenjaminDagg <bdagg@csumb.edu>
2018-09-05 02:47:45 -0700

    made results table grid view

--- src/components/ResultBar/ResultList.js
+++ src/components/ResultBar/ResultList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {VideoContainer} from "../VideoContainer/VideoContainer";
-
+import "./ResultTable.css";
 /*
 This component is the left side panel that contains
 filtrs to filter the job results
