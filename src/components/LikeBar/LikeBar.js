f91530ff23b0f0020e52bd75fc185750c4007d02
BenjaminDagg <bdagg@csumb.edu>
2018-09-05 02:34:20 -0700

    finished layout of video thumbnails

---
+++ src/components/LikeBar/LikeBar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import "./LikeBar.css";
+
+/*
+This component is the 'Job Board' logo on the top of
+the page. When clicked returns to home page
+ */
+export class LikeBar extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        var likes = parseInt(this.props.likes);
+        var dislikes = parseInt(this.props.dislikes);
+        var total = likes + dislikes;
+
+        var likeBarStyle = {
+            'width' : parseInt((likes / total) * 100).toString() + "%",
+            'height': '100%'
+
+        };
+        console.log(likeBarStyle);
+        var dislikeBarStyle = {
+            'width' : parseInt((dislikes / total) * 100).toString() + "%",
+            'height': '100%'
+
+        };
+
+        return (
+
+
+
+            <div class="likebar">
+                <div style={likeBarStyle} class="likediv">
+
+                </div>
+                <div style={dislikeBarStyle} class="dislikediv">
+
+                </div>
+            </div>
+        );
+    }
+}
+
--- src/components/VideoContainer/VideoContainer.js
+++ src/components/VideoContainer/VideoContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./VideoContainer.css";
 import { config } from "../../models/config";
 import axios from "axios";
+import { LikeBar } from "../LikeBar/LikeBar";
 
 /*
 This component is the 'Job Board' logo on the top of
@@ -16,6 +17,8 @@ export class VideoContainer extends Component {
             video: null
 
         };
+
+        this.parseNumbers = this.parseNumbers.bind(this);
 
 
     }
@@ -44,6 +47,39 @@ export class VideoContainer extends Component {
         }
     }
 
+
+    //converts a string of numbers into an easier
+    //to read format
+    /*
+    num (string) => string
+     */
+    parseNumbers(n) {
+        var n = ('' + n).split('.');
+        var num = n[0];
+        var dec = n[1];
+        var r, s, t;
+
+        if (num.length > 3) {
+            s = num.length % 3;
+
+            if (s) {
+                t = num.substring(0,s);
+                num = t + num.substring(s).replace(/(\d{3})/g, ",$1");
+            } else {
+                num = num.substring(s).replace(/(\d{3})/g, ",$1").substring(1);
+            }
+        }
+
+        if (dec && dec.length > 3) {
+            dec = dec.replace(/(\d{3})/g, "$1 ");
+        }
+
+        return num + (dec? '.' + dec : '');
+    }
+
+
+
+
     //<iframe src={this.props.url} className="player" id={this.props.video.id.videoId} type="text/html" width="200" height="200"/>
 
     render() {
@@ -57,6 +93,16 @@ export class VideoContainer extends Component {
                         </strong>
                         <br />
                         <span>{this.props.video.snippet.channelTitle}</span>
+                        <br/>
+                        <span>{this.parseNumbers(this.props.video.statistics.viewCount) + " views"}</span>
+                        <br/>
+                        <span class="likes">	&#x1f44d; {this.parseNumbers(this.props.video.statistics.likeCount)} </span>
+                        <span
+                            className="likes">	&#x1f44e; {this.parseNumbers(this.props.video.statistics.dislikeCount)} </span>
+                        <br/>
+                        <div style={{'height': '10px', "width" : '50%'}}>
+                            <LikeBar likes={this.props.video.statistics.likeCount} dislikes={this.parseNumbers(this.props.video.statistics.dislikeCount)}/>
+                        </div>
                     </p>
                 </a>
             </div>
